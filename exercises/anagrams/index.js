0eb3e92e0f986954c3dfa09d0c545efab819cfad
Darpan Rangari <darpan.rangari@lloydsbanking.com>
2021-01-28 09:17:26 +0000

    anagrams updated

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,36 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB){
-    return stringA.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('') === stringB.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('');
-}
+// function anagrams(stringA, stringB){
+//     return stringA.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('') === stringB.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('');
+// }
 
 // Solution 1
-// function anagrams(stringA, stringB) {
-//
-//     let charMapStringA = charMap(stringA);
-//     let charMapStringB = charMap(stringB);
-//
-//     if (Object.keys(charMapStringA).length !== Object.keys(charMapStringB).length){
-//         return false;
-//     }
-//
-//     for(let char in charMapStringA){
-//         if(charMapStringA[char] !== charMapStringB[char]){
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-//
-// function charMap(str){
-//     let obj = {}
-//
-//     for(let char of str){
-//         obj[char] = obj[char] + 1 || 1
-//     }
-//
-//     return obj
-// }
+function anqagrams(stringA, stringB) {
+
+    let charMapStringA = charMap(stringA);
+    let charMapStringB = charMap(stringB);
+
+    if (Object.keys(charMapStringA).length !== Object.keys(charMapStringB).length){
+        return false;
+    }
+
+    for(let char in charMapStringA){
+        if(charMapStringA[char] !== charMapStringB[char]){
+            return false;
+        }
+    }
+    return true;
+}
+
+function charMap(str){
+    let obj = {}
+
+    for(let char of str.replace(/[^\w]/g).toLowerCase()){
+        obj[char] = obj[char] + 1 || 1
+    }
+
+    return obj
+}
+
 
 module.exports = anagrams;
